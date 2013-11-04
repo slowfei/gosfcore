a746d68b42877d8c1f4fdb5440f1466911f23ca6
slowfei <slowfei@foxmail.com>
2013-11-04 16:42:14 +0800

    修改YMDHMSSS时间格式化格式为"_yyyy" to "-2006"，主要是因为 _2 是格式化日

--- utils/time/SFTimeUtil_test.go
+++ utils/time/SFTimeUtil_test.go
@@ -10,3 +10,7 @@ import (
 func TestYMDHMSSFormat(t *testing.T) {
 	fmt.Println(YMDHMSSFormat(time.Now(), "yyyy-MM-dd hh:mm:ssSSSSSSSSS -0700 MST"))
 }
+
+func TestYMDHMSSSignParse(t *testing.T) {
+	fmt.Println(YMDHMSSSignFormat(time.Now(), "_${yyyy}"))
+}
